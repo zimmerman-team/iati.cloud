70efb141120ef35b44d7b56ab60ff5462d7d4d44
José Moreira <jose@zimmermanzimmerman.nl>
2017-12-05 16:02:35 +0100

    Add title and description to CSV Activity serializer

--- OIPA/api/csvexport/serializers.py
+++ OIPA/api/csvexport/serializers.py
@@ -1,7 +1,21 @@
-from rest_framework.serializers import CharField
+from rest_framework.serializers import CharField, SerializerMethodField
 
 from api.activity.serializers import ActivitySerializer
 from iati.models import Activity
+
+
+class MySerializerMethodField(SerializerMethodField):
+    def __init__(self, method_name=None, **kwargs):
+        if 'inner_source' in kwargs:
+            self.inner_source = kwargs.pop('inner_source')
+        super(MySerializerMethodField, self).__init__(method_name, **kwargs)
+
+    def to_representation(self, value):
+        from rest_framework.fields import get_attribute
+        method = getattr(self.parent, self.method_name)
+        value = method(value)
+        val2 = get_attribute(value, self.inner_source.split('.'))
+        return val2
 
 
 class ActivityCSVExportSerializer(ActivitySerializer):
@@ -12,10 +26,40 @@ class ActivityCSVExportSerializer(ActivitySerializer):
     default_flow_type_code = CharField(source='default_flow_type.code', default='')
     default_tied_status_code = CharField(source='default_tied_status.code', default='')
 
-    reporting_org = CharField(source='reporting_organisation.organisation.primary_name', default='')
-    reporting_org_ref = CharField(source='reporting_organisation.ref', default='')
-    reporting_org_type = CharField(source='reporting_organisation.organisation.type.name', default='')
-    reporting_org_type_code = CharField(source='reporting_organisation.organisation.type.code', default='')
+    reporting_org = MySerializerMethodField(
+        method_name='get_reporting_organization',
+        inner_source='organisation.primary_name',
+        default='')
+    reporting_org_ref = MySerializerMethodField(
+        method_name='get_reporting_organization',
+        inner_source='ref',
+        default='')
+    reporting_org_type = MySerializerMethodField(
+        method_name='get_reporting_organization',
+        inner_source='organisation.type.name',
+        default='')
+    reporting_org_type_code = MySerializerMethodField(
+        method_name='get_reporting_organization',
+        inner_source='organisation.type.code',
+        default='')
+    title = SerializerMethodField(default='')
+    description = SerializerMethodField(default='')
+
+    def get_reporting_organization(self, obj):
+        return obj.reporting_organisations.first()
+
+    def get_title(self, obj):
+        narrative = obj.title.narratives.filter(language=obj.default_lang).first()
+        if narrative is not None:
+            return narrative.content
+
+    def get_description(self, obj):
+        description = obj.description_set.filter(
+            narratives__language=obj.default_lang).first()
+        if description is not None:
+            narrative = description.narratives.filter(language=obj.default_lang).first()
+            if narrative is not None:
+                return narrative.content
 
     class Meta(ActivitySerializer.Meta):
         model = Activity
@@ -37,5 +81,7 @@ class ActivityCSVExportSerializer(ActivitySerializer):
             'reporting_org',
             'reporting_org_ref',
             'reporting_org_type',
-            'reporting_org_type_code'
+            'reporting_org_type_code',
+            'title',
+            'description'
         )
