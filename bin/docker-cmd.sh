7b9ac09d3850e9518379dabb828893b6e17763bf
José Moreira <jose@zimmermanzimmerman.nl>
2018-02-05 13:15:29 +0100

    Passing env variables to uwsgi program

--- bin/docker-cmd.sh
+++ bin/docker-cmd.sh
@@ -7,5 +7,8 @@ set -e
 # generate static files
 /app/src/OIPA/manage.py collectstatic --noinput
 
+# export (docker) env vars to '/tmp/environment' file for uwsgi (ini) to read
+/usr/bin/env > /tmp/environment
+
 # run Django as a wsgi process
 /app/src/bin/wait-for-postgres.sh -- /venv/bin/uwsgi --ini /app/src/etc/uwsgi/uwsgi-production.ini
