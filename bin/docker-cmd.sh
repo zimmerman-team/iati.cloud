3d0b41354ca0852af4ce17e2d81e2e4598fd5f66
José Moreira <jose@zimmermanzimmerman.nl>
2018-02-05 11:51:55 +0100

    Move uwsgi config to file

--- bin/docker-cmd.sh
+++ bin/docker-cmd.sh
@@ -8,14 +8,4 @@ set -e
 /app/src/OIPA/manage.py collectstatic --noinput
 
 # run Django as a wsgi process
-/app/src/bin/wait-for-postgres.sh -- /venv/bin/uwsgi \
-    --chdir=/app/src/OIPA \
-    --module=OIPA.wsgi:application \
-    --env DJANGO_SETTINGS_MODULE=OIPA.settings \
-    --master --pidfile=/tmp/uwsgi.pid \
-    --http 0.0.0.0:8000 \
-    --buffer-size 32768 \
-    --processes=5 \
-    --uid=1000 --gid=1000 \
-    --vacuum \
-    --home=/venv
+/app/src/bin/wait-for-postgres.sh -- /venv/bin/uwsgi --ini /app/src/etc/uwsgi/uwsgi-production.ini
