82d06a40548404849f3a0be7c8ae5cc0bb4b2d3a
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2019-11-21 12:25:38 +0100

    Updated to use param

--- OIPA/solr/cron/backup.sh
+++ OIPA/solr/cron/backup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-rm -rf /var/solr/data/activity/data/snapshot.activity
+rm -rf /var/solr/data/$1/data/snapshot.$1
 curl --url "http://localhost:8983/solr/$1/replication?command=backup&name=$1"
